5571b4cffad34c39d42519fdc026ec6e3b76db46
sokreach <sautsokreach@gmail.com>
2026-06-16 09:58:24 +0700

    test

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -1,14 +1,23 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.Item;
-import com.example.demo.repository.ItemRepository;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.example.demo.model.Item;
+import com.example.demo.repository.ItemRepository;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/items")
@@ -57,6 +66,7 @@ public class ItemController {
             return ResponseEntity.notFound().build();
         }
 
+        
 
         
         itemRepository.deleteById(id);
